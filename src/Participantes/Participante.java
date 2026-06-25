b380b7d7cfbe7e1a7738e8b7c8bd1aa8e76d1557
Adriel <adrielvillegaslopez@gmail.com>
2026-06-25 15:55:23 -0600

    cambios a participante
    Se crea metodo get y set a disciplina

--- src/Participantes/Participante.java
+++ src/Participantes/Participante.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Participantes;
-
+import disciplinas.Disciplina;
 import Personas.Persona;
 import java.time.LocalDate;
 
@@ -12,7 +12,7 @@ import java.time.LocalDate;
  * @author Student
  */
 public class Participante extends Persona {
-
+private Disciplina Diciplina;
     @Override
     public int getId() {
         return id;
@@ -31,6 +31,15 @@ public class Participante extends Persona {
     public String getTelefono() {
         return Telefono;
     }
+
+    public Disciplina getDiciplina() {
+        return Diciplina;
+    }
+
+    public void setDiciplina(Disciplina Diciplina) {
+        this.Diciplina = Diciplina;
+    }
+    
     
     public Participante(int id, String nombre, LocalDate fecha, String Telefono) {
         super(id, nombre, fecha, Telefono);
